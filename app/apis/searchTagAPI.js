5db61144f278cc481a1e576b47809b2084c76e17
surbhiairan <surbhiairan@gmail.com>
2018-08-03 17:00:32 +0530

    search apis

--- app/apis/searchTagAPI.js
+++ app/apis/searchTagAPI.js
@@ -7,7 +7,7 @@ var async = require('async');
 
 var searchTag = require('../models/searchTagSchema');
 var restaurantSchema = require('../models/restaurantSchema');
-
+var dishRestaurantMapping = require('../models/dishRestaurantMappingSchema');
 
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({
@@ -48,13 +48,37 @@ module.exports = function searchTagAPI(app) {
         })
     });
 
-    app.get("/searchRestaurant", (req, res) => {
+    app.get("/searchRestaurants", (req, res) => {
         console.log(req.query);
-        restaurantSchema.find({"restaurant_name": {"$regex": req.query.r_name, "$options": "i"}}, function (err, results) {
+        restaurantSchema.find({ "restaurant_name": { "$regex": req.query.searchedText, "$options": "i"}}, function (err, results) {
             if(err) return res.send(err);
             console.log(results);
             res.send(results);
         })
         
+    });
+
+    app.get("/searchDishes", (req, res) => {
+        console.log(req.query);
+        searchTag.find({ "search_tag": { "$regex": req.query.searchedText, "$options": "i" } }, {_id: 0}, function (err, results) {
+            if (err) return res.send(err);
+            console.log(results);
+            res.send(results);
+        })
+
+    });
+
+    app.get("/getDishSearchResults", (req, res) => {
+        console.log(req.query);
+        dishRestaurantMapping.find(
+            { "search_tag": req.query.search_tag }, 
+            {}, 
+            { sort: { average_rating: -1 }, limit: 10 },
+            function (err, results) {
+                if (err) return res.send(err);
+                console.log(results);
+                res.send(results);
+            }
+        )
     })
 }
